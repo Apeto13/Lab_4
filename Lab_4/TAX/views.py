b747c20982f4247477fa12e68937356588453dd8
Abdul Alsalman <abdulalsalman@Abduls-MacBook-Pro.local>
2023-09-29 22:06:00 +0300

    the new app has been added plus two function

--- Lab_4/TAX/views.py
+++ Lab_4/TAX/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+tax_rate = 1.15
+
+
+def index(request):
+    return HttpResponse("this is a site to calculate tax.")
+
+
+def anyNumber(request, number):
+    numberTaxed = int(number) * tax_rate
+    return HttpResponse(f"your number is {number} and after tax is {numberTaxed}")
